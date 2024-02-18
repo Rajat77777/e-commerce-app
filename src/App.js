288381417f7f00f1ae25ea966532d191b42147a3
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-02-18 23:52:34 +0530

    Worked on SignIN, SignUP,Auth functionalities etc

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import ProductList from './features/product-list/ProductList';
 import Home from './pages/Home';
+import Login from './features/auth/components/Login';
+import LoginPage from './pages/LogInPage';
+import SignUpPage from './pages/SignUpPage';
 function App() {
   return (
     <div className="App">
-    <Home></Home>
- 
-</div>
+<SignUpPage></SignUpPage></div>
   );
 }
 
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,13 @@
+import Signup from "../features/auth/components/Signup.js";
+
+function SignUpPage(){
+
+    return (
+
+        <div>
+        <Signup></Signup>
+        </div>
+    );
+}
+
+export default SignUpPage;
