455f6bcf99ba60de503e8b17546b18f231896b85
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-02-19 22:18:09 +0530

    Added routers and cart functionality

--- src/App.js
+++ src/App.js
@@ -7,10 +7,43 @@ import Home from './pages/Home';
 import Login from './features/auth/components/Login';
 import LoginPage from './pages/LogInPage';
 import SignUpPage from './pages/SignUpPage';
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Cart from './features/cart/cart';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (<Home></Home>),
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage></SignUpPage>,
+  },
+  {
+    path: "/cart",
+    element: <Cart></Cart>,
+  },
+]);
+
+
+
 function App() {
   return (
     <div className="App">
-<SignUpPage></SignUpPage></div>
+ <RouterProvider router={router} /></div>
+ 
   );
 }
 
