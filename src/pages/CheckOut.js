ac954233aeab6c7083464e03c5ccb43d56f85f4d
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-03-01 23:58:42 +0530

    working on checkout page minor issues

--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -56,10 +56,10 @@ function CheckOut() {
     <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
         <div className="lg:col-span-3">
-          <form className="bg-white px-5 mt-12">
+          <form className="bg-white px-5 py-12 mt-12">
             <div className="space-y-12">
               <div className="border-b border-gray-900/10 pb-12">
-                <h2 className="text-base font-semibold leading-7 text-gray-900">
+                <h2 className="text-2xl font-semibold leading-7 text-gray-900">
                   Personal Information
                 </h2>
                 <p className="mt-1 text-sm leading-6 text-gray-600">
@@ -216,6 +216,21 @@ function CheckOut() {
                 </div>
               </div>
 
+              <div className="mt-6 flex items-center justify-end gap-x-6">
+              <button
+                type="button"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Save
+              </button>
+            </div>
+
               <div className="border-b border-gray-900/10 pb-12">
                 <h2 className="text-base font-semibold leading-7 text-gray-900">
                   Address
@@ -231,7 +246,7 @@ function CheckOut() {
                     >
                       <div className="flex min-w-0 gap-x-4">
                         <input
-                        name="address"
+                          name="address"
                           type="radio"
                           className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
                         />
@@ -301,25 +316,10 @@ function CheckOut() {
                 </div>
               </div>
             </div>
-
-            <div className="mt-6 flex items-center justify-end gap-x-6">
-              <button
-                type="button"
-                className="text-sm font-semibold leading-6 text-gray-900"
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Save
-              </button>
-            </div>
           </form>
         </div>
         <div className="lg:col-span-2">
-          <div className="bg-white mx-auto mt-12 max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="bg-white mx-auto mt-12 max-w-7xl px-4 sm:px-0 lg:px-0">
             <div className="bg-white border-t border-gray-200 px-4 py-6 sm:px-6">
               <h1 className="text-4xl my-5 font-bold tracking-tight text-gray-900">
                 Cart
@@ -393,7 +393,7 @@ function CheckOut() {
                   href="#"
                   className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                 >
-                  Checkout
+                  Pay and Order
                 </Link>
               </div>
               <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
