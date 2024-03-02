234140c66923d514e7e8d0187d8ebc4f554c523b
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-03-02 23:57:35 +0530

    started working on product details

--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -221,19 +221,19 @@ function CheckOut() {
                 type="button"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
-                Cancel
+                Reset
               </button>
               <button
                 type="submit"
                 className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Save
+                Add Address
               </button>
             </div>
 
               <div className="border-b border-gray-900/10 pb-12">
                 <h2 className="text-base font-semibold leading-7 text-gray-900">
-                  Address
+                  Existing Address
                 </h2>
                 <p className="mt-1 text-sm leading-6 text-gray-600">
                   Choose from Existing Address
