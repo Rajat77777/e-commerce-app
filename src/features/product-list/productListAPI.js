5080fd3838fb15399ee7754c1de058101f62992b
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-03-09 23:59:25 +0530

    working on api

--- src/features/product-list/productListAPI.js
+++ src/features/product-list/productListAPI.js
@@ -1,6 +1,7 @@
 // A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
   return new Promise(async(resolve) =>{
+    //TODO: wel will not hard-code server URL here
   const response= await fetch('http://localhost:8080');
   const data = await response.json();
   resolve({data});
