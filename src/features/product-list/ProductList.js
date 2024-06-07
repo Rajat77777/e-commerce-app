e04c2cafaf45439d1915f229f791e3640a3f523e
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-06-07 23:48:58 +0530

    pagination features in desktop and mobile layout

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -10,6 +10,8 @@ import {
   selectTotalItems,
   selectBrands,
   selectCategories,
+  fetchBrandsAsync,
+  fetchCategoriesAsync,
 } from "./productListSlice";
 
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
@@ -148,7 +150,14 @@ export default function ProductList() {
   useEffect(()=>{
     setPage(1)
 
-  },[totalItems,sort])
+  },[totalItems,sort]);
+
+  useEffect(()=>{
+    dispatch(fetchBrandsAsync());
+    dispatch(fetchCategoriesAsync());
+
+
+  },[])
 
   return (
     <div>
@@ -424,21 +433,23 @@ function DesktopFilter({handleFilter,filters}) {
   );
 }
 function Pagination(page,setPage,handlePage,totalItems) {
+  const totalPages= Math.ceil(totalItems /ITEMS_PER_PAGE);
   return (
     <div>
       <div className="flex flex-1 justify-between sm:hidden">
-        <a
-          href="#"
+        <div
+          onClick={(e)=>handlePage(page>1 ?page-1: page)}
+
           className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
         >
           Previous
-        </a>
-        <a
-          href="#"
+        </div>
+        <div
+          onClick={(e)=>handlePage(page< totalPages ?page+1: page)}
           className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
         >
           Next
-        </a>
+        </div>
       </div>
       <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
         <div>
@@ -453,16 +464,17 @@ function Pagination(page,setPage,handlePage,totalItems) {
             className="isolate inline-flex -space-x-px rounded-md shadow-sm"
             aria-label="Pagination"
           >
-            <a
-              href="#"
+            <div
+            onClick={(e)=>handlePage(page>1?page-1: page)}
+
               className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Previous</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+            </div>
            
             {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-           {Array.from({length:Math.ceil(totalItems/ITEMS_PER_PAGE)}).map((el,index)=>(
+           {Array.from({length:totalPages}).map((el,index)=>(
 
            
            <div
@@ -501,13 +513,14 @@ function Pagination(page,setPage,handlePage,totalItems) {
             >
               10
             </a>
-            <a
-              href="#"
+            <div
+            onClick={(e)=>handlePage(page>totalPages?page+1: page)}
+              
               className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               <span className="sr-only">Next</span>
               <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+            </div>
           </nav>
         </div>
       </div>
