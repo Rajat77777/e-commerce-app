f2bdb4c39df7de88f83f1466d0d237286da0ac56
Rajatsubhro <rajatsubhrochowdhury2000@gmail.com>
2024-06-19 00:03:12 +0530

    somehow

--- src/features/auth/AuthAPI.js
+++ src/features/auth/AuthAPI.js
@@ -1,7 +1,12 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
+export function createUser() {
   return new Promise(async(resolve) =>{
-  const response= await fetch('http://localhost:8080');
+  const response= await fetch('http://localhost:8080/users'{
+    method:'POST',
+    body:JSON.stringify(userData),
+    headers:{'content-type':'application/json'}
+
+  })
   const data = await response.json();
   resolve({data});
   }
